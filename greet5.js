160b408158caeae3e2432b0b34f492ed9ab700da
LeonelCeballos18 <lceballos5@ucol.mx>
2023-02-21 08:07:41 -0600

    first commit

---
+++ greet5.js
@@ -0,0 +1,19 @@
+let greeting = "Hello from greet 5";
+
+let greet = () => {
+    console.log(greeting);
+}
+
+let jump = () => {
+    console.log("im jumping");
+}
+
+let run = () => {
+    console.log("im running");
+}
+
+module.exports = {
+    greet,
+    jump,
+    run
+}
